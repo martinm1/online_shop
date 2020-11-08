69a696b467fcb009d8848d2580a5fc6ef0170b48
martin <>
2020-11-08 22:49:38 +0100

    2:4 RequestParam, PathVariable

--- onlineshop/src/main/java/org/my/application/onlineshop/controller/PageController.java
+++ onlineshop/src/main/java/org/my/application/onlineshop/controller/PageController.java
@@ -1,7 +1,9 @@
 package org.my.application.onlineshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -13,4 +15,28 @@ public class PageController {
 		mv.addObject("welcome_message", "Spring Web MVC Welcome Message");
 		return mv;
 	}
+	
+//	@RequestMapping(value="/test")
+//	public ModelAndView test(@RequestParam(value="welcome_message", required=false) String welcomeMessage) {
+//		ModelAndView mv = new ModelAndView("homepage");
+//		
+//		if(welcomeMessage == null) {
+//			welcomeMessage = "Hello world";
+//		}
+//		
+//		mv.addObject("welcome_message", welcomeMessage);
+//		return mv;
+//	}
+	
+//	@RequestMapping(value="/test/{welcome_message}")
+//	public ModelAndView test(@PathVariable(value="welcome_message", required=false) String welcomeMessage) {
+//		ModelAndView mv = new ModelAndView("homepage");
+//		
+//		if(welcomeMessage == null) {
+//			welcomeMessage = "Hello world";
+//		}
+//		
+//		mv.addObject("welcome_message", welcomeMessage);
+//		return mv;
+//	}
 }
