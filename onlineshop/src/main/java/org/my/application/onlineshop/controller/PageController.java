56db4047c591bd47f2ab78bc5bf7e16a4ba16d62
martin <>
2020-11-08 17:08:48 +0100

    2:2 dependencies and controller configuration

---
+++ onlineshop/src/main/java/org/my/application/onlineshop/controller/PageController.java
@@ -0,0 +1,16 @@
+package org.my.application.onlineshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value = {"/", "/home", "/index"})
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("homepage");
+		mv.addObject("welcome_message", "Spring Web MVC Welcome Message");
+		return mv;
+	}
+}
---
+++ onlineshop/src/main/webapp/WEB-INF/views/homepage.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Online Shop</title>
+</head>
+<body>
+	${welcome_message}
+</body>
+</html>
